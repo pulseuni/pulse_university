d9b49204281a279eb3a75a9c4620febba13a5c15
pulseuni <pantzos1234@gmail.com>
2025-05-14 18:26:10 +0300

    Add files via upload

---
+++ sql/buyer_example.sql
@@ -0,0 +1,4 @@
+USE pulse_university
+
+INSERT INTO buyers (buyer_id, buyer_name, buyer_surname, buyer_phone_number, buyer_email, buyer_age, event_id) 
+VALUES (12001, 'John', 'Doe', 1234567890, 'john.doe@example.com', 30, 2);
